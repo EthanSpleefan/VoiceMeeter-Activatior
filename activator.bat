76e22066ca0db2c8bb235007059faa5a33675fc8
Ethan <90183370+EthanSpleefan@users.noreply.github.com>
2026-01-06 14:10:14 +1000

    Enhance activator.bat with key management features

--- activator.bat
+++ activator.bat
@@ -1,26 +1,47 @@
 @echo off
+setlocal
+
 :menu
 cls
 echo 1. Add registry key
 echo 2. Remove registry key
 echo 3. Exit/Cancel
 set /p choice=Enter your choice: 
-if %choice%==1 goto addkey
-if %choice%==2 goto removekey
-if %choice%==3 goto cancel
+
+if "%choice%"=="1" goto addkey
+if "%choice%"=="2" goto removekey
+if "%choice%"=="3" goto cancel
 goto menu
 
 :addkey
-reg add "HKEY_CURRENT_USER\VB-Audio\VoiceMeeter" /v "code" /t REG_DWORD /d 0x00123456 /f
+echo Activating VoiceMeeter settings...
+
+rem Set activation code
+reg add "HKEY_CURRENT_USER\VB-Audio\VoiceMeeter" ^
+    /v "code" /t REG_DWORD /d 0x00123456 /f
+
+rem Create DelayedStartS key and set default value to 0
+reg add "HKEY_CURRENT_USER\VB-Audio\VoiceMeeter\DelayedStartS" ^
+    /ve /t REG_DWORD /d 0 /f
+
 echo Activation complete
 pause
 goto menu
 
 :removekey
-reg delete "HKEY_CURRENT_USER\VB-Audio\VoiceMeeter" /v "code" /f
+echo Removing VoiceMeeter settings...
+
+rem Remove activation code
+reg delete "HKEY_CURRENT_USER\VB-Audio\VoiceMeeter" ^
+    /v "code" /f
+
+rem Remove DelayedStartS key entirely
+reg delete "HKEY_CURRENT_USER\VB-Audio\VoiceMeeter\DelayedStartS" /f
+
 echo Deactivation complete
 pause
 goto menu
 
 :cancel
+endlocal
 exit
